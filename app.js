f15d4f4d91d52170c00afab0c18e2b091e5bb3aa
Harry Grillo <harry@redbassett.com>
2015-02-27 15:22:34 -0500

    Add bootstrap and some app logic/new forms

--- app.js
+++ app.js
@@ -9,4 +9,18 @@ angular.module('flapperNews', [])
 		{title: 'post 4', upvotes: 9},
 		{title: 'post 5', upvotes: 4}
 	];
+	
+	$scope.addPost = function(){
+		if (!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({
+			title: $scope.title
+			,link: $scope.link
+			,upvotes: 0
+		});
+		$scope.title, $scope.link = '';
+	};
+	
+	$scope.incrementUpvotes = function(post){
+		post.upvotes += 1;
+	};
 }]);
